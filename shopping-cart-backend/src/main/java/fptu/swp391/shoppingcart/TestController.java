4ac0eafc5a29d9ce869b63f574596ca4c6798146
thomas.dang <thongdh3401@gmail.com>
2023-09-15 11:17:17 +0700

    test: write some api to test authenticate and authorize

---
+++ shopping-cart-backend/src/main/java/fptu/swp391/shoppingcart/TestController.java
@@ -0,0 +1,20 @@
+package fptu.swp391.shoppingcart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    String test() {
+        return "Hello world!";
+    }
+    @GetMapping("/admin")
+    String admin() {
+        return "Hello Admin!";
+    }
+    @GetMapping("/user")
+    String user() {
+        return "Hello User!";
+    }
+}
