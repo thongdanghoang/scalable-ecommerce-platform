337ea2a3a6a10c88d93de3be64420fb1369bf343
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2023-09-16 12:53:57 +0700

    change file register.tsx

--- react/src/pages/RegisterPage/RegisterPage.tsx
+++ react/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,36 +1,84 @@
-import './register.css'
+import React,{ useState } from 'react';
+import style from './register.module.css'
+//import { User } from '../../redux/slides/userSlide';
 
 export default function RegisterPage() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  console.log(formData)
+
+  const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+  }
+
   return (
-    <div id='RegisterPage' className='background' style={{height:"100vh"}}>
-      <div className='container'>
+    <div className={style.background} style={{height:"100vh"}}>
+      <div className={style.container}>
         <h1>Register</h1>
-        <form>
-          <div className='form-control'>
-            <input type="text" id="username" placeholder="Username" />
-            <span></span>
-            <small></small>
-          </div>
-          <div className='form-control'>
-            <input type="email" id="email" placeholder="Email" />
-            <span></span>
-            <small></small>
-          </div>
-          <div className='form-control'>
-            <input type="password" id="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"  />
-            <span></span>
-            <small></small>
-          </div>
-          <div className='form-control'>
-            <input
-              type="password"
-              id="password2"
-              placeholder="Confirm password"
+        <form onSubmit={handleSignUp}>
+          <div className={style[`form-control`]}>
+            <input 
+              required 
+              type="text" 
+              name="username" 
+              value={formData.username}
+              id="username" 
+              placeholder="Username" 
+              onChange={e => setFormData({...formData,[e.target.name] : e.target.value})}
             />
             <span></span>
             <small></small>
           </div>
-          <input type="submit" value="Register" />
+          <div className={style[`form-control`]}>
+            <input 
+              required 
+              type="email" 
+              name="email"
+              value={formData.email}
+              id="email" 
+              placeholder="Email" 
+              onChange={e => setFormData({...formData,[e.target.name] : e.target.value})}
+            />
+            <span></span>
+            <small></small>
+          </div>
+          <div className={style[`form-control`]}>
+            <input 
+              required 
+              type="password"
+              name="password" 
+              value={formData.password}
+              id="password"
+              placeholder="Password" 
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+              onInvalid={(e:React.ChangeEvent<HTMLInputElement>) => e.target.setCustomValidity('Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters')}              
+              onChange={e => setFormData({...formData,[e.target.name] : e.target.value})}
+            />
+            <span></span>
+            <small></small>
+          </div>
+          <div className={style[`form-control`]}>
+            <input
+              required
+              type="password"
+              name="password2"
+              value={formData.password2}
+              id="password2"
+              pattern={`^${formData.password}$`}
+              onInvalid={(e:React.ChangeEvent<HTMLInputElement>) => e.target.setCustomValidity('Password confirm is not same password')}              
+              placeholder="Confirm password"
+              onChange={e => setFormData({...formData,[e.target.name] : e.target.value})}
+            />
+            <span></span>
+            <small></small>
+          </div>
+          <input type='submit' value="Register" />
           <div className="signup_link">Already a member? <a href="#">Login</a></div>				
         </form>
       </div>
