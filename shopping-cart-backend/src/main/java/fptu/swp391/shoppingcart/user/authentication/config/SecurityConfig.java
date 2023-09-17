4311b396dedc0ef39317bba4f8e4aabcf6956ea4
thomas.dang <thongdh3401@gmail.com>
2023-09-17 13:20:31 +0700

    feat: Add custom success and failure handlers

--- shopping-cart-backend/src/main/java/fptu/swp391/shoppingcart/user/authentication/config/SecurityConfig.java
+++ shopping-cart-backend/src/main/java/fptu/swp391/shoppingcart/user/authentication/config/SecurityConfig.java
@@ -1,18 +1,33 @@
 package fptu.swp391.shoppingcart.user.authentication.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import fptu.swp391.shoppingcart.ErrorResponse;
+import fptu.swp391.shoppingcart.user.authentication.dto.ApiResponse;
 import fptu.swp391.shoppingcart.user.authentication.service.AuthenticationProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,6 +36,56 @@ import java.util.List;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationProviderService authenticationProvider;
+
+    private static void onAuthenticationSuccess(HttpServletRequest request,
+                                                HttpServletResponse response,
+                                                Authentication authentication)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_OK); // Đặt HTTP status code là 200 OK
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        // Serialize the ApiResponse to JSON and write it to the response
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write(new ObjectMapper().writeValueAsString(
+                    new ApiResponse<>("Authentication successful", true, null)
+            ));
+            writer.flush();
+        }
+    }
+
+    private static void onAuthenticationFailure(HttpServletRequest request,
+                                                HttpServletResponse response,
+                                                AuthenticationException exception)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        errorResponse.setError("Unauthorized");
+        errorResponse.setMessage("Bad credentials");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Set the custom date format for LocalDateTime serialization
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+
+        // Serialize the ApiResponse to JSON and write it to the response
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write(objectMapper.writeValueAsString(errorResponse));
+            writer.flush();
+        }
+    }
+
+    private static void commence(HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 AuthenticationException authException)
+            throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
@@ -37,13 +102,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //@formatter:off
         http
-//                .cors().and()
-                .csrf().disable()
+                .cors().and()
+                .csrf().disable() // TODO : Enable CSRF protection for best security
                 .authorizeRequests()
                     .mvcMatchers("/admin").hasRole("ADMIN")
                     .mvcMatchers("/user").hasRole("USER")
+                    .mvcMatchers("/api/user/auth/register").permitAll()
                     .anyRequest().authenticated().and()
-                .formLogin();
+                .formLogin()
+                    .failureHandler(SecurityConfig::onAuthenticationFailure)
+                    .successHandler(SecurityConfig::onAuthenticationSuccess)
+                    .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(SecurityConfig::commence);
         //@formatter:on
     }
 
@@ -53,6 +124,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Configure allowed origins
         List<String> allowedOrigins = new ArrayList<>();
         allowedOrigins.add("http://localhost:3000"); // React dev server SWP391
+
         configuration.setAllowedOrigins(allowedOrigins);
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
