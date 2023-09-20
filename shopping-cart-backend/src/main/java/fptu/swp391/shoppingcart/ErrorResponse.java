6b9ede20731eada2f0d3c7eaa75c2bb9d3f16715
thomas.dang <thongdh3401@gmail.com>
2023-09-20 18:12:15 +0700

    refactor: rename and change location

---
+++ shopping-cart-backend/src/main/java/fptu/swp391/shoppingcart/ErrorResponse.java
@@ -0,0 +1,13 @@
+package fptu.swp391.shoppingcart;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+}
