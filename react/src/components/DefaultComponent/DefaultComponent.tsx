a02cdcceabaec01f254d458593eda24d819d5c08
thaintd <114746768+thaintd@users.noreply.github.com>
2023-09-29 17:03:01 +0700

    add footer

--- react/src/components/DefaultComponent/DefaultComponent.tsx
+++ react/src/components/DefaultComponent/DefaultComponent.tsx
@@ -1,14 +1,19 @@
-import HeaderComponent from "../HeaderComponent/HeaderComponent"
-import { ReactNode } from 'react';
+import FooterComponent from "../FooterComponent/FooterComponent";
+import HeaderComponent from "../HeaderComponent/HeaderComponent";
+import { ReactNode } from "react";
 
+const DefaultComponent: React.FC<{ children: ReactNode }> = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  return (
+    <div>
+      <HeaderComponent />
+      {children}
+      <FooterComponent />
+    </div>
+  );
+};
 
-const DefaultComponent:React.FC<{children: ReactNode}> = ({ children }: { children: ReactNode }) => {
-    return (
-      <div>
-          <HeaderComponent />
-          {children}
-      </div>
-    )
-}
-  
-export default DefaultComponent
+export default DefaultComponent;
