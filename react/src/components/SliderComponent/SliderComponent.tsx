8b8eb93593952696870e901bf0b29a46d25c655c
thaintd <114746768+thaintd@users.noreply.github.com>
2023-09-30 18:09:01 +0700

    create slider

--- react/src/components/SliderComponent/SliderComponent.tsx
+++ react/src/components/SliderComponent/SliderComponent.tsx
@@ -1,28 +1,27 @@
 import Slider from "react-slick";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Slider.css";
 interface PropsSlider {
-    slidesToShow : number,
-    listItems : Array<any>,
+  slidesToShow: number;
+  listItems: Array<any>;
 }
 
-export default function SliderComponent(props : PropsSlider) {
-    const {
-      slidesToShow = 1,
-      listItems = []
-    } = props
+export default function SliderComponent(props: PropsSlider) {
+  const { slidesToShow = 1, listItems = [] } = props;
 
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow,
-        slidesToScroll: 1
-      };
-      return (
-        <Slider {...settings}>
-          {listItems?.map(item => (
-            <div>{item}</div>
-          ))}
-        </Slider>
-      );
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+  };
+  return (
+    <Slider className="slider" {...settings}>
+      {listItems?.map((item) => (
+        <img src={item} alt="" />
+      ))}
+    </Slider>
+  );
 }
