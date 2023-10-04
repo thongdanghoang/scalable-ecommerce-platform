06abcbad6fb808d6252ea86b13ee94342ea03822
thaintd <114746768+thaintd@users.noreply.github.com>
2023-10-04 13:38:41 +0700

    update

--- react/src/routes/index.ts
+++ react/src/routes/index.ts
@@ -5,7 +5,7 @@ import ProductDetailPage from "../pages/ProductDetailPage/ProductDetailPage";
 import RegisterPage from "../pages/RegisterPage/RegisterPage";
 import ResetPasswordPage from "../pages/ResetPasswordPage/ResetPasswordPage";
 import UserProfilePage from "../pages/UserProfilePage/UserProfilePage";
-<<<<<<< HEAD
+import ProductPage from "../pages/ProductPage/ProductPage";
 
 export const routes = [
   {
@@ -38,6 +38,11 @@ export const routes = [
     page: ProductDetailPage,
     isShowHeader: true,
   },
+  {
+    path: "/product",
+    page: ProductPage,
+    isShowHeader: true,
+  },
   // {
   //     path: '/profile-user/information-user',
   //     page: InforUserComponent,
@@ -53,53 +58,3 @@ export const routes = [
     page: NotFoundPage,
   },
 ];
-=======
-import ProductPage from "../pages/ProductPage/ProductPage";
-
-export const routes = [
-    {
-        path: '/',
-        page: HomePage,
-        isShowHeader: true
-    },
-    {
-        path: '/sign-in',
-        page: LoginPage,
-        isShowHeader: false
-    },
-    {
-        path: '/sign-up',
-        page: RegisterPage,
-        isShowHeader: false
-    },
-    {
-        path: '/reset-password',
-        page: ResetPasswordPage,
-        isShowHeader: false,
-    },
-    {
-        path: '/profile-user',
-        page: UserProfilePage,
-        isShowHeader: true
-    },
-    {
-        path: '/product',
-        page: ProductPage,
-        isShowHeader: true
-    },
-    // {
-    //     path: '/profile-user/information-user',
-    //     page: InforUserComponent,
-    //     isShowHeader: true
-    // },
-    // {
-    //     path: '/profile-user/order-user',
-    //     page: OrderHistoryComponent,
-    //     isShowHeader: true
-    // },
-    {
-        path: '*',
-        page: NotFoundPage
-    }
-]
->>>>>>> e0b896090426454e2e4b968033da2311ebffd49e
