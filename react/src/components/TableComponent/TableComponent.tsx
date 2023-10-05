e84d52febd772b3a2fde8ff012763118fee942fc
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2023-10-05 15:00:10 +0700

    mock category

---
+++ react/src/components/TableComponent/TableComponent.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { Table } from "antd";
+import LoadingComponent from '../../components/LoadingComponent/LoadingComponent'
+
+interface propsTable {
+    columns : Array<any>,
+    listData : Array<any>,
+    isLoading : boolean,
+    onRow : any,
+    pageSize : number,
+    isRowSelection : true,
+    setListApproveOrder : any
+}
+
+export default function TableComponent(props : propsTable) {
+    const {
+        columns = [] , 
+        listData = [] , 
+        isLoading = false , 
+        onRow , 
+        pageSize = 6 , 
+        isRowSelection = true , 
+        setListApproveOrder = ''
+    } = props;
+
+    const dataSource = listData?.map(data => ({...data, key : data._id}))
+
+    return (
+        <LoadingComponent isloading={isLoading} delay={500}>
+            <Table
+                rowSelection={isRowSelection && {
+                    type: "checkbox",
+                    onChange: (selectedRowKeys, selectedRows) => {
+                        setListApproveOrder && setListApproveOrder(selectedRows)
+                        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+                    },
+                }}
+                columns={columns}
+                dataSource={dataSource}
+                onRow={onRow}
+                bordered={true}
+                pagination={{
+                    pageSize,
+                }}
+                
+            />
+        </LoadingComponent>
+    )
+}
