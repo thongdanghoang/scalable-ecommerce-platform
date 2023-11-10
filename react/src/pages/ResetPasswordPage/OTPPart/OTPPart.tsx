d74675210744e38347f40a9f9e25340f14bf5ba4
nghiaha <nghiaha.22022003@gmail.com>
2023-11-10 20:13:25 +0700

    fix OTP page

--- react/src/pages/ResetPasswordPage/OTPPart/OTPPart.tsx
+++ react/src/pages/ResetPasswordPage/OTPPart/OTPPart.tsx
@@ -3,40 +3,39 @@ import OTPInput from "react-otp-input";
 import "./otp.css";
 import { ResetPasswordPart } from "../ResetPasswordPart/ResetpasswordPart";
 import { forgetPasswordByMail } from "../../../services/userService";
+import { toast } from "react-toastify";
+import { Modal } from "antd";
+import { useNavigate } from "react-router-dom";
 
 export function OTPPart({ email }: any) {
-
-  const handleAPI = async (otp: string): Promise<string | null> => {
+  const handleAPI = async (otp: string) => {
     let response = await forgetPasswordByMail(email, otp);
     if (response != null) {
-      console.log(await response.json())
-      if (response.status == 200) {
+      if (response.ok) {
         setCurrentForm(() => {
           return <ResetPasswordPart />;
         });
       } else {
-        let errMess = await response.json();
-        return errMess.message;
+        toast.error("OTP không chính xác!");
       }
     }
-    return null;
   };
 
   const OTPForm = () => {
     const [otp, setOtp] = useState("");
-    const [validateOTP, setValidateOTP] = useState({
-      invalidOTP: false,
-      errorOTPMessage: "",
-    });
+    const [times, setTimes] = useState(1);
+    const navigate = useNavigate();
 
     const handleSubmit = async () => {
-      let result = await handleAPI(otp);
-      if (result != null) {
-      setValidateOTP({
-        invalidOTP: true,
-        errorOTPMessage: result
-      })
-    }
+      if (times == 6) {
+        Modal.error({
+          title: "Bạn nhập sai OTP quá 5 lần!",
+          content: "Quay lại trang trước.",
+          onOk: () => navigate(0),
+        });
+      }
+      setTimes(times + 1);
+      await handleAPI(otp);
     };
     return (
       <>
@@ -45,16 +44,14 @@ export function OTPPart({ email }: any) {
           className="d-flex align-items-center justify-content-center"
         >
           <div className="container p-3 mb-5 rounded d-flex flex-column align-items-center justify-content-around">
-            <div className="text-center">Chúng tôi đã gửi mã OTP đến email của bạn</div>
+            <div className="text-center">
+              Chúng tôi đã gửi mã OTP đến email của bạn
+            </div>
             <p>Vui lòng kiểm tra email của bạn và nhập mã OTP tại đây</p>
             <OTPInput
               value={otp}
               onChange={(event) => {
                 setOtp(event);
-                setValidateOTP({
-                  invalidOTP: false,
-                  errorOTPMessage: ""
-                })
               }}
               numInputs={6}
               inputType="number"
@@ -63,8 +60,7 @@ export function OTPPart({ email }: any) {
               containerStyle={"otp-input-container"}
               inputStyle={"otp-input shadow"}
             />
-            <div >
-              <p className="text-danger">{validateOTP.invalidOTP ? validateOTP.errorOTPMessage: null}</p>
+            <div>
               <button
                 className={`btn btn-primary ${
                   otp.length != 6 ? "disabled" : null
@@ -85,4 +81,3 @@ export function OTPPart({ email }: any) {
   });
   return CurrentForm;
 }
-
