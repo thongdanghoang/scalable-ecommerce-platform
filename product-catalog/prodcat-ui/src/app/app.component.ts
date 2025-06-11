b07871117ae17d0118225ed3e848228dc1103c40
Thống <thongdhse160015@fpt.edu.vn>
2025-06-11 23:03:31 +0700

    oblique
    npm config set legacy-peer-deps true
    npm i js-cookie
    npm i @angular/cdk@19
    npm i @angular/animations@19
    ob new prodcat-ui

--- product-catalog/prodcat-ui/src/app/app.component.ts
+++ product-catalog/prodcat-ui/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+	selector: 'app-root',
+	template: `
+		<h1>Welcome to {{ title }}!</h1>
 
-    <router-outlet />
-  `,
-  standalone: false,
-  styles: []
+		<router-outlet />
+	`,
+	standalone: false,
+	styles: []
 })
 export class AppComponent {
-  title = 'prodcat-ui';
+	title = 'prodcat-ui';
 }
--- product-catalog/prodcat-ui/src/app/app.module.ts
+++ product-catalog/prodcat-ui/src/app/app.module.ts
@@ -1,18 +1,63 @@
-import { NgModule, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule, provideExperimentalZonelessChangeDetection} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {
+	OB_BANNER,
+	ObButtonModule,
+	ObExternalLinkModule,
+	ObHttpApiInterceptor,
+	ObMasterLayoutConfig,
+	ObMasterLayoutModule,
+	provideObliqueConfiguration
+} from '@oblique/oblique';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
+import localeZHCN from '@angular/common/locales/fr-CH';
+import localeVIVN from '@angular/common/locales/it-CH';
+import localeENUS from '@angular/common/locales/de-CH';
+import {TranslateModule} from '@ngx-translate/core';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {HomeComponent} from './home/home.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+registerLocaleData(localeZHCN);
+registerLocaleData(localeVIVN);
+registerLocaleData(localeENUS);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [provideExperimentalZonelessChangeDetection()],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, HomeComponent],
+	imports: [
+		AppRoutingModule,
+		ObMasterLayoutModule,
+		BrowserAnimationsModule,
+		ObButtonModule,
+		TranslateModule,
+		MatButtonModule,
+		MatCardModule,
+		MatIconModule,
+		ObExternalLinkModule
+	],
+	providers: [
+		provideExperimentalZonelessChangeDetection(),
+		{provide: LOCALE_ID, useValue: 'zh-CN'},
+		provideObliqueConfiguration({
+			accessibilityStatement: {
+				applicationName: 'Product Catalog',
+				applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted',
+				contact: {/* at least 1 email or phone number has to be provided */ emails: [''], phones: ['']}
+			}
+		}),
+		{provide: HTTP_INTERCEPTORS, useClass: ObHttpApiInterceptor, multi: true},
+		provideHttpClient(withInterceptorsFromDi()),
+		{provide: OB_BANNER, useValue: environment.banner}
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+	constructor(config: ObMasterLayoutConfig) {
+		config.locale.locales = ['zh-CN', 'vi-VN', 'en-US'];
+	}
+}
