5abb5d3469a182e6a90610f5612a88d4e9e68982
Thống <thongdhse160015@fpt.edu.vn>
2025-06-11 22:47:17 +0700

    npx @angular/cli@19 new --experimental-zoneless --minimal --skip-tests --routing --ssr=false --standalone=false --style=scss prodcat-ui

---
+++ product-catalog/prodcat-ui/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+
+    <router-outlet />
+  `,
+  standalone: false,
+  styles: []
+})
+export class AppComponent {
+  title = 'prodcat-ui';
+}
---
+++ product-catalog/prodcat-ui/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [provideExperimentalZonelessChangeDetection()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
