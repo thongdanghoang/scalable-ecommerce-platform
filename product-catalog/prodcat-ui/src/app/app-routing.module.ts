555ea609f0439384c16fa0bec5e3775d9563465b
Thống <thongdhse160015@fpt.edu.vn>
2025-06-12 23:19:48 +0700

    squashed:
    fine tune
     npx @angular/cli@19 add @oblique/oblique@13 --title="prodcat-ui" --locales="de-CH fr-CH it-CH" --environments="local dev ref test abn prod" --prefix="app" --proxy=" " --no-ajv --unknownRoute --httpInterceptors --banner --externalLink --no-jest --no-protractor --no-npmrc --sonar --eslint --husky
    ob new prodcat-ui
    npm i js-cookie
    npm i @angular/cdk@19
    npm i @angular/animations@19

---
+++ product-catalog/prodcat-ui/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {UnknownRouteComponent} from './unknown-route/unknown-route.component';
+
+const routes: Routes = [
+	{path: '', redirectTo: 'home', pathMatch: 'full'},
+	{path: 'home', component: HomeComponent},
+	{path: '**', component: UnknownRouteComponent}
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ product-catalog/prodcat-ui/src/app/unknown-route/unknown-route.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-unknown-route',
+	standalone: false,
+	templateUrl: './unknown-route.component.html',
+	styleUrl: './unknown-route.component.scss'
+})
+export class UnknownRouteComponent {}
