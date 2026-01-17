84edf4e19a907e3303194a483a3a861851f50fa5
thongdanghoang <info@thongdanghoang.id.vn>
2026-01-17 21:47:45 +0700

    Refactor Payment Service: Update Gradle dependencies, replace StartupEvent with scheduled processing, and enhance payment initialization logic.

--- code/payment-service/src/main/java/vn/id/thongdanghoang/sep/payment_service/PaymentResource.java
+++ code/payment-service/src/main/java/vn/id/thongdanghoang/sep/payment_service/PaymentResource.java
@@ -1,17 +1,18 @@
 package vn.id.thongdanghoang.sep.payment_service;
 
+import java.time.Instant;
 import java.util.Random;
 import java.util.UUID;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.event.Observes;
 
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
-import io.quarkus.runtime.StartupEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.quarkus.scheduler.Scheduled;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,19 +31,16 @@ public class PaymentResource {
      * Sends message to the "words-out" channel, can be used from a JAX-RS resource or any bean of your application.
      * Messages are sent to the broker.
      **/
-    void onStart(@Observes StartupEvent ev) throws InterruptedException, JsonProcessingException {
-        while (true) {
-            var min = 1000;
-            var max = 5000;
-            var delayMillis = random.nextInt(max - min + 1) + min;
-            Thread.sleep(delayMillis);
-            var txId = UUID.randomUUID().toString();
-            var amount = delayMillis * 100; // 100k - 500k
-            var voucher = random.nextBoolean() ? "SAVE50" : "FREESHIP"; // Random voucher
-
-            var event = new PaymentInitiated(txId, "user-" + random.nextInt(100), amount, voucher);
-            log.info(">> [PAYMENT] Init: {}", objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(event));
-            emitter.send(event);
-        }
+    @Scheduled(every = "1s", delayed = "1s")
+    void onStart() throws JsonProcessingException {
+        var min = 1000;
+        var max = 5000;
+        var delayMillis = random.nextInt(max - min + 1) + min;
+        var txId = UUID.randomUUID().toString();
+        var amount = delayMillis * 100; // 100k - 500k
+        var voucher = random.nextBoolean() ? "SAVE50" : "FREESHIP"; // Random voucher
+        var event = new PaymentInitiated(txId, "user-" + Instant.now().toEpochMilli(), amount, voucher);
+        log.info(">> [PAYMENT] Init: {}", objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(event));
+        emitter.send(event);
     }
 }
