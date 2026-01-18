91f6b463057717005e2f22c1d2f30a2970ed7d53
thongdanghoang <info@thongdanghoang.id.vn>
2026-01-18 21:25:29 +0700

    Refactor Wallet Service: Migrate from Gradle to Maven, replace associated files, remove Java records, delete test SQL scripts, and update resource and project structure.

--- code/wallet-service/src/main/java/vn/id/thongdanghoang/sep/wallet/PaymentInitiated.java
+++
@@ -1,8 +0,0 @@
-package vn.id.thongdanghoang.sep.wallet;
-
-public record PaymentInitiated(
-        String transactionId,
-        String userId,
-        double originalAmount,
-        String voucherCode
-) {}
